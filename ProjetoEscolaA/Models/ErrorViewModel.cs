179c58d8d0dbd6c31e6df5a009850f2238181b59
Dangar-dev <danielsobraldev@gmail.com>
2025-05-29 14:54:21 -0300

    1 - Criando o Projeto Escolar

---
+++ ProjetoEscolaA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoEscolaA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
