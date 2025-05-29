715932f1e628d93eeff07fb0bdde5ef92ae4f1e8
Dangar-dev <danielsobraldev@gmail.com>
2025-05-29 15:13:32 -0300

    6- Criando a Model Aluno

---
+++ ProjetoEscolaA/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoEscolaA.Models
+{
+    public class Aluno
+    {
+        public int CodAluno { get; set; }
+
+        public string NomeAluno { get; set; }
+
+        public int Idade { get; set; }
+
+    }
+}
