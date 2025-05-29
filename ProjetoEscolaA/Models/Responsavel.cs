263430d715249c7c4d43cf0919cf94e3c19eaadc
Dangar-dev <danielsobraldev@gmail.com>
2025-05-29 15:16:33 -0300

    7- Criando a model Responsavel

---
+++ ProjetoEscolaA/Models/Responsavel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoEscolaA.Models
+{
+    public class Responsavel
+    {
+        public int CodResp {  get; set; }
+
+        public int CodAluno { get; set; }
+
+        public string NomeResp { get; set; }
+
+        public Aluno Aluno { get; set; }
+    }
+}
