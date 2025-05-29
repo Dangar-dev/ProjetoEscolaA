a963baef1009c9e053848005ac9682d234d74356
Dangar-dev <danielsobraldev@gmail.com>
2025-05-29 14:58:07 -0300

    2- Criando a controller Escola

---
+++ ProjetoEscolaA/Controllers/EscolaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoEscolaA.Controllers
+{
+    public class EscolaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
